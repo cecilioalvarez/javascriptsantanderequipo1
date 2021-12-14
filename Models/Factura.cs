583abe055eb61b10bfd49ff4ee83f24280b94a86
Naza2332 <namave23@hotmail.com>
2021-12-14 13:25:08 +0100

    crear Factura.cs

---
+++ Models/Factura.cs
@@ -0,0 +1,14 @@
+public class Factura
+{
+
+    public int Numero { get; set; }
+    public string Concepto { get; set; }
+    public int Importe { get; set; }
+
+    public Factura(int Numero, string Concepto, int Importe)
+    {
+        this.Numero=Numero;
+        this.Concepto= Concepto;
+        this.Importe=Importe;
+    }
+}
