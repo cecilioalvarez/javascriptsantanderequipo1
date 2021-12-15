41a81b79f0ac6f5d7016414b633230efcef6b110
Miguel Labrador <github4138@mlsdm.com>
2021-12-15 13:37:42 +0100

    Añadido método borrar

--- Models/Repositorio/FacturaRepositorio.cs
+++ Models/Repositorio/FacturaRepositorio.cs
@@ -5,39 +5,58 @@ using System.Collections.Generic;
 namespace javascriptsantanderequipo1.Models
 
 {
-    public class FacturaRepository {
+    public class FacturaRepository
+    {
 
-        public List<Factura> BuscarTodos() {
+        public List<Factura> BuscarTodos()
+        {
 
 
             string connectionString = "datasource=localhost;port=3306;username=root;database=cursosantander;SslMode=none";
-                // Tu consulta en SQL
-                string query = "SELECT * from Facturas";
-                List<Factura> lista= new List<Factura>();
-            
-                    MySqlConnection databaseConnection = new MySqlConnection(connectionString);
-                    MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
-                    databaseConnection.Open();
-                    MySqlDataReader reader;
-                    reader = commandDatabase.ExecuteReader();
-                    while (reader.Read()) {
-                        
-                        Factura factura= new Factura(reader.GetInt32("Numero"),reader.GetString("concepto"),reader.GetDecimal("Importe"));
-                        lista.Add(factura);
-                    }
+            // Tu consulta en SQL
+            string query = "SELECT * from Facturas";
+            List<Factura> lista = new List<Factura>();
 
-                return lista;
-
-        }
-        public void Insertar(Factura f){
-            string connectionString = "datasource=localhost;port=3306;username=root;database=cursosantander;SslMode=none";
-            
-            string query = "insert into Facturas values ("+ f.Numero+ ", '" + f.Concepto+ "'," +f.Importe+")"; 
-             
-            
             MySqlConnection databaseConnection = new MySqlConnection(connectionString);
             MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
-            
+            databaseConnection.Open();
+            MySqlDataReader reader;
+            reader = commandDatabase.ExecuteReader();
+            while (reader.Read())
+            {
+
+                Factura factura = new Factura(reader.GetInt32("Numero"), reader.GetString("concepto"), reader.GetDecimal("Importe"));
+                lista.Add(factura);
+            }
+
+            return lista;
+
+        }
+        public void Insertar(Factura f)
+        {
+            string connectionString = "datasource=localhost;port=3306;username=root;database=cursosantander;SslMode=none";
+
+            string query = "insert into Facturas values (" + f.Numero + ", '" + f.Concepto + "'," + f.Importe + ")";
+
+
+            MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+
+            databaseConnection.Open();
+            commandDatabase.CommandText = query;
+            commandDatabase.ExecuteNonQuery();
+        }
+
+        public void Borrar(Factura f)
+        {
+            string connectionString = "datasource=localhost;port=3306;username=root;database=cursosantander;SslMode=none";
+
+            string query = "delete from Facturas where numero='" + f.Numero + "'";
+
+
+            MySqlConnection databaseConnection = new MySqlConnection(connectionString);
+            MySqlCommand commandDatabase = new MySqlCommand(query, databaseConnection);
+
             databaseConnection.Open();
             commandDatabase.CommandText = query;
             commandDatabase.ExecuteNonQuery();
